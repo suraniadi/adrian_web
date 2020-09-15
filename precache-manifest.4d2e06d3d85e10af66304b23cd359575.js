43d8767dbe7eadcbfafe49667beb1b9c6248618e
Surani Adrian-Petru <surani@Suranis-MacBook-Air.local>
2020-09-16 00:05:41 +0300

    Updates

---
+++ precache-manifest.4d2e06d3d85e10af66304b23cd359575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4442b4e8ec90881441b99d108f35ee1e",
+    "url": "/adrian_web/index.html"
+  },
+  {
+    "revision": "951072c986f469110060",
+    "url": "/adrian_web/static/js/2.a011838f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adrian_web/static/js/2.a011838f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304cca92092cb168424c",
+    "url": "/adrian_web/static/js/main.d9069d57.chunk.js"
+  },
+  {
+    "revision": "2af8a18edf102986e7bd",
+    "url": "/adrian_web/static/js/runtime-main.f15072eb.js"
+  }
+]);
